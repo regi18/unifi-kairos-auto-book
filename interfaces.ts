6333ac54db57c664036443c01f410c9777f77a75
Simone Carletti <regi18@outlook.it>
2021-09-21 09:39:58 +0200

    Update interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -3,3 +3,19 @@ export interface AppConfig {
     password: string;
     fiscalCode: string;
 }
+
+export interface Lecture {
+  PostoOccupato: any;
+  PresenzaAula: any;
+  aula: string;
+  capacita: number;
+  entry_id: number;
+  last_minute: boolean;
+  nome: string;
+  note: string;
+  ora_fine: string;
+  ora_inizio: string;
+  posto: number;
+  prenotabile: boolean;
+  prenotata: boolean;
+}
